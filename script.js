8fe46f38a34a72cb184d8581b4f58b0af345d3f0
DantesSin616 <asimpleperson100@gmail.com>
2025-02-03 12:53:56 -0400

    yeah..

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-
-
 function getComputerChoice(){
-    return "rock", "paper", "scissors";
+    const choices = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * 3); 
+    return choices[randomNumber];
 }
+
+function getHumanChoice() {
+    prompt("Rock, paper or Scissors? ")
+}
+
+function ComputerScore() {
+    let computerScore = 0;
+    // If statement.
+}
+
+
+console.log(getComputerChoice());
